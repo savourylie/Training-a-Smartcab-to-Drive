1cf2d55a905e7ea6975583de77cebaba9c58df9e
Calvin Ku <calvin.j.ku@googlemail.com>
2016-05-21 04:07:15 +0800

    Added None action compatibility (TBC).
    TODO: 1. Fix nnext_headings
          2. Remove all numpy matrix types

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -11,7 +11,7 @@ import numpy as np
 
 
 
-class LearningAgent(Agent):
+class LearningAgent(Agent): ## TODO: REMOVE EVERY MATRIX FROM THIS CLASS!!!!!!!!!
     """An agent that learns to drive in the smartcab world."""
 
     r90_matrix = np.matrix([[0, -1], [1, 0]]) # Rotate CCW
@@ -26,21 +26,46 @@ class LearningAgent(Agent):
         # TODO: Initialize any additional variables here
         self.epsilon = 1
         self.gamma = 0.9
-        self.i_alpha = 1
+        self.i_alpha = 2
         self.q_dict = defaultdict(lambda: np.random.choice(self.random_reward)) # element of q_dict is (loc_x, loc_y, hd_x, hd_y, tl, oa_oc, oa_lt, oa_rt, act): q_value
 
-    def q_learning(self, location, heading, traffic_light, other_agents):
+    def max_q(self, location, heading, traffic_light, other_agents):
         max_q = ''
         q_compare_dict = {}
 
-        for loc_x, loc_y, hd_x, hd_y, tl, oa_oc, oa_lt, oa_rt, act in self.q_dict:
+        # Populate the q_dict
+        for act in set(Environment.valid_actions):
+            _ = self.q_dict[(location[0], location[1], heading[0], heading[1], traffic_light, other_agents[0], other_agents[1], other_agents[2], act)]
+
+        for loc_x, loc_y, hd_x, hd_y, tl, oa_oc, oa_lt, oa_rt, act in self.q_dict: # <- DEFAULTDICT doesn't work here!!!!!!
             if (loc_x == location[0] and loc_y == location[1] and hd_x == heading[0] and hd_y == heading[1] and tl == traffic_light and oa_oc == other_agents[0] and oa_lt == other_agents[1] and oa_rt == other_agents[2]):
+                "YES!!"
                 q_compare_dict[(loc_x, loc_y, hd_x, hd_y, tl, oa_oc, oa_lt, oa_rt, act)] = self.q_dict[(loc_x, loc_y, hd_x, hd_y, tl, oa_oc, oa_lt, oa_rt, act)]
-        key, q_value = max(q_compare_dict.iteritems(), key=lambda x:x[1])
+                # print("q_compare_dict (loop): {}".format(q_compare_dict))
 
-        return key[-1], q_value
+        try:
+            max(q_compare_dict.iteritems(), key=lambda x:x[1])
+        except ValueError:
+            pass
+            # print("No Qs for the state, yet...")
+        else:
+            key, q_value = max(q_compare_dict.iteritems(), key=lambda x:x[1])
+            return key[-1], q_value
 
-    def _navigation(self, heading, delta):
+    def q_update(self, now_loc, now_heading, now_light, now_agents):
+        q_temp = self.q_dict[(self.prev_loc[0], self.prev_loc[1], self.prev_heading[0], self.prev_heading[1], self.prev_light, self.prev_agents[0], self.prev_agents[1], self.prev_agents[2], self.prev_action)]
+        # print("q_temp: {}".format(q_temp))
+        # print("prev_reward: {}".format(self.prev_reward))
+        # print("max_q: {}".format(self.max_q(now_loc, now_heading, now_light, now_agents)))
+        # print("Gamma: {}".format(self.gamma))
+        # print("Alpha: {}".format(1 / self.i_alpha))
+        q_temp = (1 - (1 / self.i_alpha)) * q_temp + (1 / self.i_alpha) * (self.prev_reward + self.gamma * self.max_q(now_loc, now_heading, now_light, now_agents)[1])
+        self.q_dict[(self.prev_loc[0], self.prev_loc[1], self.prev_heading[0], self.prev_heading[1], self.prev_light, self.prev_agents[0], self.prev_agents[1], self.prev_agents[2], self.prev_action)] = q_temp
+        self.i_alpha = self.i_alpha + 1
+        return (self.q_dict[(self.prev_loc[0], self.prev_loc[1], self.prev_heading[0], self.prev_heading[1],
+            self.prev_light, self.prev_agents[0], self.prev_agents[1], self.prev_agents[2], self.prev_action)])
+
+    def _navigator(self, heading, delta):
         """Use heading and delta (next loc - current loc) to decide the SmartCab's action. Internal use (by policy) only.
         """
         if delta[0] == 0 and delta[1] == 0:
@@ -67,45 +92,66 @@ class LearningAgent(Agent):
         next_headings = [np.matrix(heading), np.dot(self.r90_matrix, heading), np.dot(self.rn90_matrix, heading)]
         print("Next Headings: {}".format(next_headings))
 
+
         next_locs = [x + np.matrix(location) for x in next_headings]
+        next_locs.append(np.matrix(location))
+
+        print("NEXT LOCS!: {}".format(next_locs))
         temp_next_loc = []
-        # print("Next Locations (RAW): {}".format(next_locs))
+        print("Next Locations (RAW): {}".format(next_locs))
 
         for m in next_locs:
             temp_next_loc.append(np.matrix(coord_convert([m[0, 0], m[0, 1]])))
 
         next_locs = temp_next_loc
-
         print("Next Locations: {}".format(next_locs))
 
-        valid_actions = set(['forward', 'right', 'left'])
+        ## TODO: FIX THE COORDINATE PROBLEMS FOR nnext_headings!!!!
+        nnext_headings1 = [(loc - np.matrix(location)).T for loc in next_locs]
+        nnext_headings2 = [np.dot(self.r90_matrix, (loc - np.matrix(location)).T) for loc in next_locs]
+        nnext_headings3 = [np.dot(self.rn90_matrix, (loc - np.matrix(location)).T) for loc in next_locs]
+        nnext_headings = nnext_headings1 + nnext_headings2 + nnext_headings3
+        temp_nnext_headings = []
+        print("NNext headings (RAW): {}".format(nnext_headings))
+
+        for m in nnext_headings:
+            # print(m)
+            temp_nnext_headings.append(np.matrix(delta_convert([m[0, 0], m[1, 0]])))
+
+        print(temp_nnext_headings)
+        # nnext_headings = temp_nnext_headings
+
+        valid_actions = set([None, 'forward', 'right', 'left'])
 
         max_q = ''
         q_compare_dict = {}
 
+        # print("q_dict: {}".format([x for x in self.q_dict.values()]))
+        # print("q_dict: {}".format([x for x in self.q_dict]))
+        print("Test MaxQ: {}".format(self.max_q([3, 4], (-1, 0), 'red', ['forward', None, None])))
         for loc in next_locs:
-            for hd in [(loc - np.matrix(location)).T, np.dot(self.r90_matrix, (loc - np.matrix(location)).T), np.dot(self.rn90_matrix, (loc - np.matrix(location)).T)]:
+            print("Next loc (in loop): {}".format(loc))
+            for hd in nnext_headings:
+                # print("NNext headings: {}".format(hd))
                 for tl in set(['green', 'red']):
                     for oa_oc in valid_actions:
                         for oa_lt in valid_actions:
                             for oa_rt in valid_actions:
-                                for act in set(Environment.valid_actions):
-                                    q_compare_dict[(loc[0, 0], loc[0, 1], hd[0, 0], hd[1, 0], tl, oa_oc, oa_lt, oa_rt, act)] = self.q_dict[(loc[0, 0], loc[0, 1], hd[0, 0], hd[1, 0], tl, oa_oc, oa_lt, oa_rt, act)]
-
+                                # if self.max_q([loc[0, 0], loc[0, 1]], (hd[0, 0], hd[1, 0]), tl, [oa_oc, oa_lt, oa_rt]) is not None:
+                                q_compare_dict[(loc[0, 0], loc[0, 1], hd[0, 0], hd[1, 0], tl, oa_oc, oa_lt, oa_rt)] = self.max_q([loc[0, 0], loc[0, 1]], (hd[0, 0], hd[1, 0]), tl, [oa_oc, oa_lt, oa_rt])[1]
+        # print("q_compare_dict: {}".format(q_compare_dict))
         key, q_value = max(q_compare_dict.iteritems(), key=lambda x:x[1])
 
-        print(key[-1], q_value)
+        print("Very useful KEY!: {}".format(key))
+        print("Useless MaxQ: {}".format(q_value))
 
-        delta = [(key[0] - location[0]) % 2, (key[1] - location[1]) % 2]
+        delta = [(key[0] - location[0]), (key[1] - location[1])]
         delta[0], delta[1] = delta_convert([delta[0], delta[1]])
 
         print("delta[0] = {}".format(delta[0]))
         print("delta[1] = {}".format(delta[1]))
 
-        print("delta[0] * heading[1]  = {}".format(delta[0] * heading[1]))
-        print("delta[1] * heading[0]  = {}".format(delta[1] * heading[0]))
-
-        return self._navigation(heading, delta)
+        return self._navigator(heading, delta)
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -150,10 +196,7 @@ class LearningAgent(Agent):
         except NameError:
             print("Initializing...no prevs exist.")
         else:
-            q_update = q_dict[(self.prev_loc[0], self.prev_loc[1], self.prev_heading[0], self.prev_heading[1], self.prev_light, self.prev_agents[0], self.prev_agents[1], self.prev_agents[2], self.prev_action)]
-            q_update = (1 - alpha) * q_update + (alpha * (self.gamma * self.q_learning(now_loc, now_heading, now_light, now_agents)))
-            q_dict[(self.prev_loc[0], self.prev_loc[1], self.prev_heading[0], self.prev_heading[1], self.prev_light, self.prev_agents[0], self.prev_agents[1], self.prev_agents[2], self.prev_action)] = q_update
-            self.i_alpha = self.i_alpha + 1
+            self.q_update(now_loc, now_heading, now_light, now_agents)
 
         self.prev_loc = now_loc
         self.prev_heading = now_heading
@@ -161,7 +204,6 @@ class LearningAgent(Agent):
         self.prev_agents = now_agents
         self.prev_action = action
         self.prev_reward = reward
-
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
--- smartcab/smartcab/test_agent.py
+++ smartcab/smartcab/test_agent.py
@@ -6,14 +6,13 @@ from environment import Environment
 import numpy as np
 from numbers import Number
 
+
 class TestAgent:
 	def setup_method(self, method):
 		print("Setting up METHOD {0}".format(method.__name__))
 		self.e = Environment()
 		self.agent = self.e.create_agent(LearningAgent)
 		self.e.set_primary_agent(self.agent, enforce_deadline=False)
-
-		self.q_dict = self.agent.q_dict
 
 	def test_q_dict(self):
 		# q_dict is (loc_x, loc_y, hd_x, hd_y, tl, oa_oc, oa_lt, oa_rt, act): q_value
@@ -22,54 +21,202 @@ class TestAgent:
 		assert self.agent.q_dict[28] in [-2, -1, 0, 1, 2]
 		assert self.agent.q_dict[100] not in [3, 5, -100]
 
-	def test_q_learning_output_type(self):
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
-		assert isinstance(self.agent.q_learning(np.array([5, 6]), np.array([1, 0]), 'green', ['forward', 'right', 'left'])[1], Number)
+	def test_max_q_output_type(self):
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
+		assert isinstance(self.agent.max_q([5, 6], [1, 0], 'green', ['forward', 'right', 'left'])[1], Number)
 
-	def test_q_learning_output_value(self):
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
+	def test_max_q_output_value(self):
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
 
-		assert self.agent.q_learning(np.array([5, 6]), np.array([1, 0]), 'green', ['forward', 'right', 'left'])[1] == 18
+		assert self.agent.max_q([5, 6], [1, 0], 'green', ['forward', 'right', 'left'])[1] == 18
 
-	def test__navigation(self):
-		assert self.agent._navigation((1, 0), [0, -1]) == 'left'
-		assert self.agent._navigation((1, 0), [0, 1]) == 'right'
-		assert self.agent._navigation((1, 0), [1, 0]) == 'forward'
-		assert self.agent._navigation((0, 1), [0, 1]) == 'forward'
-		assert self.agent._navigation((0, 1), [1, 0]) == 'left'
-		assert self.agent._navigation((0, 1), [-1, 0]) == 'right'
-		assert self.agent._navigation((0, 1), [0, 0]) == None
-		assert self.agent._navigation((0, -1), [0, 0]) == None
-		assert self.agent._navigation((1, 0), [0, 0]) == None
-		assert self.agent._navigation((-1, 0), [0, 0]) == None
-		assert self.agent._navigation((-1, 0), [-1, 0]) == 'forward'
-		assert self.agent._navigation((-1, 0), [0, 1]) == 'left'
-		assert self.agent._navigation((-1, 0), [0, -1]) == 'right'
-		assert self.agent._navigation((0, -1), [0, -1]) == 'forward'
-		assert self.agent._navigation((0, -1), [-1, 0]) == 'left'
-		assert self.agent._navigation((0, -1), [1, 0]) == 'right'
+	def test_q_update_output_value(self):
+		# Prev state/action 1
+		self.agent.prev_reward = 5
+		self.agent.prev_loc = [4, 6]
+		self.agent.prev_heading = (1, 0)
+		self.agent.prev_light = 'green'
+		self.agent.prev_agents = ['right', 'left', 'forward']
+		self.agent.prev_action = 'forward'
+
+		self.agent.q_dict[(self.agent.prev_loc[0], self.agent.prev_loc[1], self.agent.prev_heading[0], self.agent.prev_heading[1], self.agent.prev_light, self.agent.prev_agents[0], self.agent.prev_agents[1], self.agent.prev_agents[2], self.agent.prev_action)] = 2
+
+		# Now state 1
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
+
+		# Possibilities 1
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
+
+		assert self.agent.q_update([5, 6], [1, 0], 'green', ['forward', 'right', 'left']) \
+		== 11.6
+
+		self.agent.i_alpha = 5
+		self.agent.gamma = 0.9
+
+		# Prev state 2
+		self.agent.prev_reward = 2
+		self.agent.prev_loc = [8, 6]
+		self.agent.prev_heading = (0, 1)
+		self.agent.prev_light = 'green'
+		self.agent.prev_agents = [None, None, 'right']
+		self.agent.prev_action = 'left'
+
+		self.agent.q_dict[(self.agent.prev_loc[0], self.agent.prev_loc[1], self.agent.prev_heading[0], self.agent.prev_heading[1], self.agent.prev_light, self.agent.prev_agents[0], self.agent.prev_agents[1], self.agent.prev_agents[2], self.agent.prev_action)] = -1
+
+		# Now state 2
+		now_loc = [1, 6]
+		now_heading = (1, 0)
+		now_light = 'red'
+		now_agents = [None, 'forward', None]
+
+		# Possibilities 2
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], 'forward')] = 3
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], 'right')] = 2
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], 'left')] = 0
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], None)] = -1
+
+		assert self.agent.q_update(now_loc, now_heading, now_light, now_agents) \
+		== 0.14
+
+		self.agent.i_alpha = 20
+		self.agent.gamma = 0.9
+
+		# Prev state 3
+		self.agent.prev_reward = 5
+		self.agent.prev_loc = [4, 4]
+		self.agent.prev_heading = (-1, 0)
+		self.agent.prev_light = 'red'
+		self.agent.prev_agents = [None, 'right', 'forward']
+		self.agent.prev_action = 'forward'
+
+		self.agent.q_dict[(self.agent.prev_loc[0], self.agent.prev_loc[1], self.agent.prev_heading[0], self.agent.prev_heading[1], self.agent.prev_light, self.agent.prev_agents[0], self.agent.prev_agents[1], self.agent.prev_agents[2], self.agent.prev_action)] = -2.5
+
+		# Now state 3
+		now_loc = [3, 4]
+		now_heading = (-1, 0)
+		now_light = 'red'
+		now_agents = ['forward', 'forward', None]
+
+		# Possibilities 3
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], 'forward')] = -1
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], 'right')] = 2
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], 'left')] = -2
+		self.agent.q_dict[(now_loc[0], now_loc[1], now_heading[0], now_heading[1], now_light, now_agents[0], now_agents[1], now_agents[2], None)] = -1
+
+		assert self.agent.q_update(now_loc, now_heading, now_light, now_agents) \
+		== -2.035
+
+	def test__navigator(self):
+		assert self.agent._navigator((1, 0), [0, -1]) == 'left'
+		assert self.agent._navigator((1, 0), [0, 1]) == 'right'
+		assert self.agent._navigator((1, 0), [1, 0]) == 'forward'
+		assert self.agent._navigator((0, 1), [0, 1]) == 'forward'
+		assert self.agent._navigator((0, 1), [1, 0]) == 'left'
+		assert self.agent._navigator((0, 1), [-1, 0]) == 'right'
+		assert self.agent._navigator((0, 1), [0, 0]) == None
+		assert self.agent._navigator((0, -1), [0, 0]) == None
+		assert self.agent._navigator((1, 0), [0, 0]) == None
+		assert self.agent._navigator((-1, 0), [0, 0]) == None
+		assert self.agent._navigator((-1, 0), [-1, 0]) == 'forward'
+		assert self.agent._navigator((-1, 0), [0, 1]) == 'left'
+		assert self.agent._navigator((-1, 0), [0, -1]) == 'right'
+		assert self.agent._navigator((0, -1), [0, -1]) == 'forward'
+		assert self.agent._navigator((0, -1), [-1, 0]) == 'left'
+		assert self.agent._navigator((0, -1), [1, 0]) == 'right'
 
 	def test_policy_output_type(self):
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
-		self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
+		self.agent.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
 
-		assert type(self.agent.policy(np.array([5, 6]), np.array([1, 0]), 'green', ['forward', 'right', 'left'])) == type('string')
+		assert type(self.agent.policy(np.array([5, 6]), np.array([1, 0]), 'green', ['forward', 'right', 'left'])) == type('string') or type(None)
 
-	# def test_policy_output_value(self):
-	# 	self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'right')] = 18
-	# 	self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'forward')] = -2
-	# 	self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', None)] = 0
-	# 	self.q_dict[(5, 6, 1, 0, 'green', 'forward', 'right', 'left', 'left')] = 12
+	def test_policy_output_value(self):
+		# Now state 1
+		now_loc = [3, 4]
+		now_heading = (-1, 0)
+		now_light = 'red'
+		now_agents = ['forward', 'forward', None]
 
-	# 	assert self.agent.policy(np.array([5, 6]), np.array([1, 0]), 'green', ['forward', 'right', 'left']) == 18
+		# Next state 1-0
+		next_loc10 = [3, 4]
+		next_heading10 = (-1, 0)
+		next_light10 = 'red'
+		next_agents10 = ['forward', None, None]
+		next_action10 = 'right'
+
+		# Next state 1-1-1
+		next_loc11 = [3, 3]
+		next_heading11 = (0, -1)
+		next_light11 = 'red'
+		next_agents11 = ['forward', 'forward', None]
+		next_action11 = None
+
+		# Next state 1-1-2
+		next_loc12 = [3, 3]
+		next_heading12 = (0, -1)
+		next_light12 = 'green'
+		next_agents12 = ['forward', None, None]
+		next_action12 = 'forward'
+
+		# Next state 1-2-1
+		next_loc21 = [2, 4]
+		next_heading21 = (-1, 0)
+		next_light21 = 'green'
+		next_agents21 = ['left', 'forward', None]
+		next_action21 = 'left'
+
+		# Next state 1-2-2
+		next_loc22 = [2, 4]
+		next_heading22 = (-1, 0)
+		next_light22 = 'red'
+		next_agents22 = ['left', 'forward', 'left']
+		next_action22 = 'right'
+
+		# Next state 1-2-3
+		next_loc23 = [2, 4]
+		next_heading23 = (-1, 0)
+		next_light23 = 'green'
+		next_agents23 = ['forward', 'forward', 'forward']
+		next_action23 = 'right'
+
+		# Next state 1-3
+		next_loc3 = [3, 5]
+		next_heading3 = (0, 1)
+		next_light3 = 'red'
+		next_agents3 = ['forward', None, None]
+		next_action3 = 'right'
+
+		# Possibilities 1
+		self.agent.q_dict[(next_loc11[0], next_loc11[1], next_heading11[0], next_heading11[1], next_light11, next_agents11[0], next_agents11[1], next_agents11[2], next_action11)] = 3
+		self.agent.q_dict[(next_loc12[0], next_loc11[1], next_heading12[0], next_heading12[1], next_light12, next_agents12[0], next_agents12[1], next_agents12[2], next_action12)] = 1
+		self.agent.q_dict[(next_loc21[0], next_loc21[1], next_heading21[0], next_heading21[1], next_light21, next_agents21[0], next_agents21[1], next_agents21[2], next_action21)] = 0
+		self.agent.q_dict[(next_loc22[0], next_loc22[1], next_heading22[0], next_heading22[1], next_light22, next_agents22[0], next_agents22[1], next_agents22[2], next_action22)] = -1.5
+		self.agent.q_dict[(next_loc23[0], next_loc23[1], next_heading23[0], next_heading23[1], next_light23, next_agents23[0], next_agents23[1], next_agents23[2], next_action23)] = -1.5
+		self.agent.q_dict[(next_loc3[0], next_loc3[1], next_heading3[0], next_heading3[1], next_light3, next_agents3[0], next_agents3[1], next_agents3[2], next_action3)] = -3
+
+		assert self.agent.policy(now_loc, now_heading, now_light, now_agents) == 'right'
+
+		self.agent.q_dict[(next_loc3[0], next_loc3[1], next_heading3[0], next_heading3[1], next_light3, next_agents3[0], next_agents3[1], next_agents3[2], next_action3)] = 10
+
+		assert self.agent.policy(now_loc, now_heading, now_light, now_agents) == 'left'
+
+		self.agent.q_dict[(next_loc22[0], next_loc22[1], next_heading22[0], next_heading22[1], next_light22, next_agents22[0], next_agents22[1], next_agents22[2], next_action22)] = 15
+
+		assert self.agent.policy(now_loc, now_heading, now_light, now_agents) == 'forward'
+
+		self.agent.q_dict[(next_loc10[0], next_loc10[1], next_heading10[0], next_heading10[1], next_light10, next_agents10[0], next_agents10[1], next_agents10[2], next_action10)] = 20
+
+		assert self.agent.policy(now_loc, now_heading, now_light, now_agents) == None
 
 def test_coord_convert():
 	assert coord_convert([8, 5]) == [8, 5]
@@ -81,6 +228,7 @@ def test_coord_convert():
 	assert coord_convert([0, 3]) == [8, 3]
 	assert coord_convert([0, 0]) == [8, 6]
 	assert coord_convert([2, 0]) == [2, 6]
+
 
 def test_delta_convert():
 	assert delta_convert([0, -5]) == [0, 1]
